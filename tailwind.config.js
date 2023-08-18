7c05ebe92071d678b218f58cb89ef77f0bb99b2d
= <=>
2023-08-18 12:50:36 +0300

    theme configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      cyan: {
+        dark: '#5ba4a4',
+      },
+      'grayish-cyan': {
+        light: '#eef6f6',
+        medium: '#7b8e8e',
+        dark: '#2c3a3a',
+      },
+      white: colors.white,
+      black: colors.black,
+    },
+    extend: {
+      fontFamily: {
+        'league-sparton': "'League Spartan', sans-serif;",
+      },
+      fontSize: {
+        base: '0.938rem',
+      },
+    },
   },
   plugins: [],
-}
-
+};
